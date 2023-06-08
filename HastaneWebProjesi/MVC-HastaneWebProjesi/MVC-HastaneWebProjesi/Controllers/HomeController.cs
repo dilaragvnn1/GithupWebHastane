1f50e31c7d3e7ad54ecdf5b703c8640d5385fc9b
Dilara Güven <95206434+dilaragvnn1@users.noreply.github.com>
2023-06-08 19:58:00 +0300

    Hastane Projem
    Projelerim arasında en keyif aldığım çalışmaydı. Bu çalışmamda sizlere statik uzantı kullanmadan tasarım odaklı bir web projesi yapmaya çalıştım.

---
+++ HastaneWebProjesi/MVC-HastaneWebProjesi/MVC-HastaneWebProjesi/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_HastaneWebProjesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Baslik = "Ana Sayfa";
+            ViewBag.AnaSayfa = "active";
+            return View();
+        }
+        public ActionResult İletisim()
+        {
+            ViewBag.Baslik = "İletişim Sayfası";
+            ViewBag.İletisim = "active";
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            ViewBag.Baslik = "Hakkimizda Sayfası";
+            ViewBag.Hakkimizda = "active";
+            return View();
+        }
+        public ActionResult Servis()
+        {
+            ViewBag.Baslik = "Servis Sayfası";
+            ViewBag.Servis = "active";  
+            return View();  
+        }
+        public ActionResult Fiyatlandirma()
+        {
+            ViewBag.Baslik = "Fiyatlandirma Sayfası";
+            ViewBag.Fiyatlandirma = "active";
+            return View() ; 
+        }
+    }
+}
